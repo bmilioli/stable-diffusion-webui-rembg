61fe73e986965bf5055954eec2ea348fe5c6d8de
Bmilioli <101820274+bmilioli@users.noreply.github.com>
2023-06-24 21:40:20 -0300

    Update api.py

--- scripts/api.py
+++ scripts/api.py
@@ -19,7 +19,7 @@ import rembg
 def rembg_api(_: gr.Blocks, app: FastAPI):
     @app.post("/rembg")
     async def rembg_remove(
-        input_image: str = Body("", title='rembg input image'),
+        input_images: List[str] = Body([], title='rembg input images'),
         model: str = Body("u2net", title='rembg model'), 
         return_mask: bool = Body(False, title='return mask'), 
         alpha_matting: bool = Body(False, title='alpha matting'), 
@@ -30,19 +30,23 @@ def rembg_api(_: gr.Blocks, app: FastAPI):
         if not model or model == "None":
             return
 
-        input_image = api.decode_base64_to_image(input_image)
+        images = [api.decode_base64_to_image(img) for img in input_images]
 
-        image = rembg.remove(
-            input_image,
-            session=rembg.new_session(model),
-            only_mask=return_mask,
-            alpha_matting=alpha_matting,
-            alpha_matting_foreground_threshold=alpha_matting_foreground_threshold,
-            alpha_matting_background_threshold=alpha_matting_background_threshold,
-            alpha_matting_erode_size=alpha_matting_erode_size,
-        )
+        results = []
+        for input_image in images:
+            image = rembg.remove(
+                input_image,
+                session=rembg.new_session(model),
+                only_mask=return_mask,
+                alpha_matting=alpha_matting,
+                alpha_matting_foreground_threshold=alpha_matting_foreground_threshold,
+                alpha_matting_background_threshold=alpha_matting_background_threshold,
+                alpha_matting_erode_size=alpha_matting_erode_size,
+            )
+            results.append(api.encode_pil_to_base64(image).decode("utf-8"))
 
-        return {"image": api.encode_pil_to_base64(image).decode("utf-8")}
+        return {"images": results}
+
 
 try:
     import modules.script_callbacks as script_callbacks
